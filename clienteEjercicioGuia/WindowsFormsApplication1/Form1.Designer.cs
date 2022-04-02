3a2acd54973a4c25f5062af4dfbeff7d16f10516
Ferran Ortega de la Mata <ferran.ortega@estudiantat.upc.edu>
2022-04-02 17:54:59 +0200

    versionaccesoexcluyente

--- clienteEjercicioGuia/WindowsFormsApplication1/Form1.Designer.cs
+++ clienteEjercicioGuia/WindowsFormsApplication1/Form1.Designer.cs
@@ -30,12 +30,17 @@
         {
             this.label2 = new System.Windows.Forms.Label();
             this.nombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.Longitud = new System.Windows.Forms.RadioButton();
+            this.altura = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.alturaBox = new System.Windows.Forms.TextBox();
             this.Bonito = new System.Windows.Forms.RadioButton();
-            this.alto = new System.Windows.Forms.RadioButton();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contLbl = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -57,9 +62,20 @@
             this.nombre.Size = new System.Drawing.Size(164, 20);
             this.nombre.TabIndex = 3;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(25, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(130, 144);
+            this.button2.Location = new System.Drawing.Point(126, 157);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
@@ -70,17 +86,17 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.alto);
             this.groupBox1.Controls.Add(this.Longitud);
+            this.groupBox1.Controls.Add(this.altura);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.alturaBox);
             this.groupBox1.Controls.Add(this.Bonito);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.nombre);
-            this.groupBox1.Location = new System.Drawing.Point(12, 114);
+            this.groupBox1.Location = new System.Drawing.Point(23, 104);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(363, 282);
+            this.groupBox1.Size = new System.Drawing.Size(363, 202);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Peticion";
@@ -95,7 +111,33 @@
             this.Longitud.TabStop = true;
             this.Longitud.Text = "Dime la longitud de mi nombre";
             this.Longitud.UseVisualStyleBackColor = true;
-            this.Longitud.CheckedChanged += new System.EventHandler(this.Longitud_CheckedChanged);
+            // 
+            // altura
+            // 
+            this.altura.AutoSize = true;
+            this.altura.Location = new System.Drawing.Point(116, 119);
+            this.altura.Name = "altura";
+            this.altura.Size = new System.Drawing.Size(98, 17);
+            this.altura.TabIndex = 7;
+            this.altura.TabStop = true;
+            this.altura.Text = "Dime si soy alto";
+            this.altura.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Altura";
+            // 
+            // alturaBox
+            // 
+            this.alturaBox.Location = new System.Drawing.Point(15, 110);
+            this.alturaBox.Name = "alturaBox";
+            this.alturaBox.Size = new System.Drawing.Size(62, 20);
+            this.alturaBox.TabIndex = 9;
             // 
             // Bonito
             // 
@@ -107,49 +149,62 @@
             this.Bonito.TabStop = true;
             this.Bonito.Text = "Dime si mi nombre es bonito";
             this.Bonito.UseVisualStyleBackColor = true;
-            this.Bonito.CheckedChanged += new System.EventHandler(this.Bonito_CheckedChanged);
             // 
-            // alto
+            // button3
             // 
-            this.alto.AutoSize = true;
-            this.alto.Location = new System.Drawing.Point(116, 114);
-            this.alto.Name = "alto";
-            this.alto.Size = new System.Drawing.Size(98, 17);
-            this.alto.TabIndex = 9;
-            this.alto.TabStop = true;
-            this.alto.Text = "Dime si soy alto";
-            this.alto.UseVisualStyleBackColor = true;
-            this.alto.CheckedChanged += new System.EventHandler(this.alto_CheckedChanged);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(25, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 53);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "desconectar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // textBox1
+            // button4
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 216);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 20);
-            this.textBox1.TabIndex = 10;
+            this.button4.Location = new System.Drawing.Point(437, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 40);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Num. servicios";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // contLbl
+            // 
+            this.contLbl.AutoSize = true;
+            this.contLbl.Location = new System.Drawing.Point(471, 223);
+            this.contLbl.Name = "contLbl";
+            this.contLbl.Size = new System.Drawing.Size(0, 13);
+            this.contLbl.TabIndex = 12;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(23, 211);
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(423, 214);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 25);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Altura";
+            this.label1.Size = new System.Drawing.Size(134, 68);
+            this.label1.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(741, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.contLbl);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,13 +212,18 @@
 
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton Longitud;
         private System.Windows.Forms.RadioButton Bonito;
+        private System.Windows.Forms.RadioButton altura;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox alturaBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label contLbl;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton alto;
     }
 }
 
--- servidorEjercicioGuia.c
+++ servidorEjercicioGuia.c
@@ -5,13 +5,98 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <pthread.h>
 
-int main(int argc, char *argv[])
+int cont;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void *AtenderCliente (void *socket)
 {
-	int sock_conn, sock_listen, ret;
-	struct sockaddr_in serv_adr;
+	int sock_conn;
+	int *s;
+	s= (int *) socket;
+	sock_conn= *s;
+	
 	char peticion[512];
 	char respuesta[512];
+	int ret;
+	
+	int terminar =0;
+	// Entramos en un bucle para atender todas las peticiones de este cliente
+	//hasta que se desconecte
+	while (terminar ==0)
+	{
+		ret=read(sock_conn,peticion, sizeof(peticion));
+		printf ("Recibido\n");
+		
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		peticion[ret]='\0';
+		
+		
+		printf ("Peticion: %s\n",peticion);
+		
+		// vamos a ver que quieren
+		char *p = strtok( peticion, "/");
+		int codigo =  atoi (p);
+		// Ya tenemos el c?digo de la petici?n
+		char nombre[20];
+		if (codigo !=0)&&(codigo != 4)
+		{
+			p = strtok( NULL, "/");
+			
+			strcpy (nombre, p);
+			// Ya tenemos el nombre
+			printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+		}
+		
+		if (codigo ==0) //petici?n de desconexi?n
+			terminar=1;
+		
+		else if (codigo ==1) //piden la longitd del nombre
+			sprintf (respuesta,"%d",strlen (nombre));
+		else if (codigo == 2)
+			// quieren saber si el nombre es bonito
+			if((nombre[0]=='M') || (nombre[0]=='S'))
+			strcpy (respuesta,"SI");
+			else
+				strcpy (respuesta,"NO");
+		
+		else if (codigo == 4)
+			sprintf (respuesta, "%d ",cont);
+			
+		else 
+				p = strtok(NULL, "/");
+			float altura =  atof (p);
+			if(altura > 1.70)
+				sprintf (respuesta, "%s: eres alto",nombre);
+			else
+				sprintf (respuesta, "%s: eres bajo",nombre);
+			
+			
+		
+		
+		if ((codigo==1)||(codigo==2)||(codigo==3))
+		{
+			pthread_mutex_lock(&mutex);
+			cont = cont +1;
+			pthread_mutex_unlock(&mutex);
+		}
+		
+			
+		printf (respuesta);
+			
+		write  (sock_conn, respuesta,strlen(respuesta));
+		//write (sock_conn,resultado,strlen(resultado));
+		close(sock_conn); 
+	}
+
+	
+int main(int argc, char *argv[])
+{
+		
+	int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+		
 	// INICIALITZACIONS
 	// Obrim el socket
 	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -29,53 +114,27 @@ int main(int argc, char *argv[])
 	//La cola de peticiones pendientes no podr? ser superior a 4
 	if (listen(sock_listen, 4) < 0)
 		printf("Error en el Listen");
-	int i;
-	// Atenderemos solo 10 peticione
-	for(i=0;i<10;i++){
+	
+	cont = 0;
+	int i=0;
+	int sockets[100];
+	pthread_t thread;
+	for(;;)
+	{
 		printf ("Escuchando\n");
 		
 		sock_conn = accept(sock_listen, NULL, NULL);
 		printf ("He recibido conexi?n\n");
+		sockets[i]=sock_conn;
 		//sock_conn es el socket que usaremos para este cliente
 		
-		// Ahora recibimos su peticion
-		ret=read(sock_conn,peticion, sizeof(peticion));
-		printf ("Recibida una petición\n");
-		// Tenemos que a?adirle la marca de fin de string 
-		// para que no escriba lo que hay despues en el buffer
-		peticion[ret]='\0';
-		
-		//Escribimos la peticion en la consola
-		
-		printf ("La petición es: %s\n",peticion);
-		char *p = strtok(peticion, "/");
-		int codigo =  atoi (p);
-		p = strtok( NULL, "/");
-		char nombre[20];
-		strcpy (nombre, p);
-		printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
-		
-		if (codigo ==1) //piden la longitd del nombre
-			sprintf (respuesta,"%d",strlen (nombre));
-		else if (codigo == 2)
-			// quieren saber si el nombre es bonito
-			if((nombre[0]=='M') || (nombre[0]=='S'))
-			strcpy (respuesta,"SI");
-			else
-				strcpy (respuesta,"NO");
-			
-		else
-			p = strtok(NULL, "/");
-		    float altura =  atof (p);
-				if(altura > 1.70)
-					stpcpy(respuesta,"Eres alto");
-				else
-					strcpy(respuesta,"Eres bajo");
-				
-		// Enviamos la respuesta
-		write (sock_conn,respuesta, strlen(respuesta));
-			
-		// Se acabo el servicio para este cliente
-		close(sock_conn); 
+		pthread_create(&thread,NULL, AtenderCliente,&sockets[i]);
+		i ++;
+	
 	}
+	
 }
+	
+	
+	
+
